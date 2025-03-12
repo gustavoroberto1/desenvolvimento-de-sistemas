3aa8122f6a1ef9595ecefa831f590398bd5e6086
ROSANE MAIA BETINE <rosane_maia@estudante.sc.senai.br>
2025-03-11 22:01:12 -0300

    semana06

--- semana06/src/app/login/page.tsx
+++ semana06/src/app/login/page.tsx
@@ -1,13 +1,52 @@
 import Link from "next/link";
+import "./Styles.css";
+import { useState } from "react";
+import { useRouter } from "next/navigation";
 
-export default function Home(){
+export default function login(){
+    const [email,setEmail] = useState<string>('');
+    const [password,setPassword] = useState<string>('');
+    const rouper = useRouter();
+
+    function handleSubmit(){
+        console.log(email)
+        console.log(password)
+
+      //  exemplo requisicã
+       // const respose = await fetch("http://meu-dominio/login",){
+       //     method: "post",
+       //     body: {
+        //        email,
+       //         password
+        //    }as any,
+      //  }
+
+    }
     return (
-        <div>
-            <h1>Home</h1>
-            <Link href="/">
-                <button>Home</button>
-            </Link>
+        <div className="container">
+            <div className="form">
+                <h2>LOGIN</h2>
+                <input 
+                      type="text"
+                      placeholder="E-mail"
+                      className="input"
+                      value={email}
+                      onChange={(event)=> setEmail(event.target.value)}
+                />
+                <input 
+                      type="password" 
+                      placeholder="Password" 
+                      className="input"
+                      value={password}
+                      onChange={(event)=> setPassword(event.target.value)}
+
+                />
+                <button className="button"
+                onClick={handleSubmit}
+                disabled={!email || !password}
+                >Entrar</button>
             </div>
+        </div>
         
     )
 }
--- semana06/src/app/page.tsx
+++ semana06/src/app/page.tsx
@@ -1,36 +1,61 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+'use client'
+
 import Link from "next/link";
 import Person from "@/components/person";
+import "./home.css";
+import { useState } from "react";
 
 export default function Home() {
-  const lista = [
-    {nome: "Rosane",
-      age: 42},
-
-      {
-        nome: "Luiza",
-        age: 23
-      }
-  ]
+  //let contador = 0;
+  const [contador, setContador] = useState<number>(0)
+  
+  function incrementar(){
+    setContador(contador + 1);
+  }
+  
+  function resetar(){
+    setContador(0);
+  }
   return (
-    <div>
-
-      < h1>HELLO WORLD</h1>
-      <h2>teste</h2>
-
-
-      <Link href="/login">
-        <button>login</button>
-      </Link>
-      <div>
-        <div className= "contanier">
-          {lista.map((item)=>( <Person nome={item.nome} idade={item.age} key={item.nome}/>
-          ))}
-          
-        </div>
-
-      </div>
+    <div className="container">
+      <h1>contador:{contador}</h1>
+      <button onClick={()=>incrementar()}>incrementar </button>
+      <button onClick={() => resetar()} >resetar</button>
     </div>
-  );
+
+);
 }
+
+
+
+// const lista = [
+   // {
+   //   nome: "Rosane",
+  //    age: 42
+   // },
+
+   // {
+   //   nome: "Luiza",
+   //   age: 23
+    //}
+// ]
+
+
+   // <div>
+
+     // < h1>HELLO WORLD</h1>
+     // <h2>teste</h2>
+
+
+     // <Link href="/login">
+    //     <button>Home</button>
+    //   </Link>
+    //   <div>
+    //     <div className= "contanier">
+    //       {lista.map((item)=>( <Person nome={item.nome} idade={item.age} key={item.nome}/>
+    //       ))}
+          
+    //     </div>
+
+    //   </div>
+    // </div>
