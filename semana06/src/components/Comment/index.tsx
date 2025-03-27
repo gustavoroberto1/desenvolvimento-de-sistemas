82fd8f4cd2d0c9f334d161767e0d981393ff51fc
ROSANE MAIA BETINE <rosane_maia@estudante.sc.senai.br>
2025-03-27 19:51:58 -0300

    semana08

--- semana06/src/components/Comment/index.tsx
+++ semana06/src/components/Comment/index.tsx
@@ -1,60 +1,57 @@
-import { PiThumbsUp, PiTrash } from 'react-icons/pi';
+import { formatDistanceToNow } from 'date-fns';
 import Avatar from '../Avatar';
-import'./styles.css';
+import './styles.css';
+import { PiThumbsUp, PiTrash } from 'react-icons/pi';
+import { ptBR } from 'date-fns/locale';
 
 type Author = {
     name: string;
     role: string;
     avatarUrl: string;
-    
 }
 
 type CommentProps = {
-    handleLike:(event:MouseEvent,id:string) =>void;
-    handleDelete:
-    comment:{
-    id: string;
-    comment: string,
-    publishedAt: Date,
-    like: number,
-    author: Author
+    handleLike: (event: any, id: string) => void;
+    handleDelete: (event: any, id: string) => void;
+    comment: {
+        id: string;
+        like: number;
+        author: Author;
+        comment: string;
+        publishedAt: Date
+    }
 }
-}
-
-
-
-export default function Comment({comment}:CommentProps){
-    const dateFormat = formatDistanceToNow(post.publishedAt, {
+export default function Comment({ comment, handleDelete, handleLike }: CommentProps) {
+    const dateFormat = formatDistanceToNow(comment.publishedAt, {
         locale: ptBR,
         addSuffix: true
     })
+
     return (
         <div className='comment'>
-            <Avatar src={ comment.author.avatarUrl}hasBorder={false}/>
+            <Avatar src={comment.author.avatarUrl} hasBorder={false} />
             <div className='comment-box'>
                 <div className='comment-content'>
                     <header>
                         <div className='author-and-time'>
-                            <strong>comment.author.name"</strong>
+                            <strong>{comment.author.name}</strong>
                             <time>{dateFormat}</time>
-
                         </div>
-                        <button title='Deletar comentario'>
-                            <PiTrash size={24}/>
+                        <button title='Deletar comentário' onClick={(event) => handleDelete(event, comment.id)}>
+                            <PiTrash size={24} />
                         </button>
                     </header>
-                    <p>È isso mesmo...só falou verdades</p>
-
+                    <p>{comment.comment}</p>
                 </div>
                 <footer>
-                    <button>
-                        <PiThumbsUp/>
-                        Curtir <span>12</span>
+                    <button onClick={(event) => handleLike(event, comment.id)}>
+                        <PiThumbsUp />
+                        Apludir <span>{comment.like}</span>
                     </button>
                 </footer>
             </div>
-            
         </div>
+
     )
- 
 }
+
--- semana06/src/components/Post/index.tsx
+++ semana06/src/components/Post/index.tsx
@@ -1,35 +1,37 @@
 'use client'
+
 import { Dispatch, FormEvent, SetStateAction, useState } from "react";
 import Avatar from "../Avatar";
+import { v4 as uuid } from 'uuid';
 import "./Styles.css";
-import { format, formatDistanceToNow } from "date-fns";
+import { formatDistanceToNow } from "date-fns";
 import { ptBR } from "date-fns/locale";
-import TextareaCustom from "../TextareaCustom/Index";
+
+import ButtonCustom from "../ButtonCustom";
 import axios from "axios";
 import Comment from "../Comment";
-import {v4 as uuid} from 'uuid';
+import TextareaCustom from "../TextareaCustom/Index";
 
 type Author = {
     name: string;
     role: string;
     avatarUrl: string;
-    author: Author;
 }
 
 type Comment = {
     id: string;
-    comment: string,
-    publishedAt: Date,
-    like: number,
-    author: Author
+    like: number;
+    author: Author;
+    comment: string;
+    publishedAt: Date
 }
 
 type Post = {
     id: string;
     author: Author;
-    publishedAt: Date,
-    content: string,
-    comments: Comment[];
+    publishedAt: Date;
+    content: string;
+    comments: Comment[]
 }
 
 type PostProps = {
@@ -40,24 +42,15 @@ type PostProps = {
 export default function Post({ post, setPost }: PostProps) {
     const [newComment, setNewComment] = useState<string>('');
 
-
     async function loadPost() {
-
-    }
-    async function handleDeletComment(event:MouseEvent, id:string) {
-        event.preventDefault();
-        const comentsFilter = post.comments.filter
-        
-    }
-    async function handlelikeComment(event:MouseEvent) {
-        
-    }
-    const 
-
         const response = await axios.get(`http://localhost:3001/posts/${post.id}`);
+
+        // ATUALIZA POSIÇÃO ESPECIFICA DO ESTADO
         setPost((prev: Post[]) =>
             prev.map(atual => (
-                atual.id == post.id ? response.data : atual)))
+                atual.id === post.id ? response.data : atual
+            ))
+        )
     }
 
     async function handleCreateNewComment(event: FormEvent) {
@@ -70,18 +63,45 @@ export default function Post({ post, setPost }: PostProps) {
             like: 0,
             author: {
                 name: "Rosane",
-                role: "personal organizer",
+                role: "Personal organizer",
                 avatarUrl: "https://avatars.githubusercontent.com/u/170477548?v=4&size=64"
             }
         }
+
         const comments = post.comments?.length ? [...post.comments, comment] : [comment]
 
         await axios.patch(`http://localhost:3001/posts/${post.id}`, {
             "comments": comments
         })
-        loadPost()
-        setNewComment('');
 
+        loadPost();
+        setNewComment('');
+    }
+
+    async function handleDeleteComment(event: MouseEvent, id: string) {
+        event.preventDefault();
+        const comentsFilter = post.comments.filter(comment => comment.id !== id);
+        await axios.patch(`http://localhost:3001/posts/${post.id}`, {
+            "comments": comentsFilter
+        })
+        
+        loadPost();
+    }
+
+    async function handleLikeComment(event: MouseEvent, id: string) {
+        event.preventDefault();
+        const commentUpdated = post.comments.map(comment => {
+            if (comment.id === id) {
+                return { ...comment, like: comment.like + 1 };
+            }
+            return comment;
+        })
+
+        await axios.patch(`http://localhost:3001/posts/${post.id}`, {
+            "comments": commentUpdated
+        })
+
+        loadPost();
     }
 
     const dateFormat = formatDistanceToNow(post.publishedAt, {
@@ -95,39 +115,43 @@ export default function Post({ post, setPost }: PostProps) {
                 <div className="author">
                     <Avatar src={post.author.avatarUrl} hasBorder />
                     <div className="author-info">
-                        <span>{post.author.name}</span>
+                        <strong>{post.author.name}</strong>
                         <span>{post.author.role}</span>
                     </div>
                 </div>
+
                 <time>
                     {dateFormat}
                 </time>
             </header>
+
             <div className="content">
-                <p>
-                    
-                    {post.content}
-
-                </p>
+                <p>{post.content}</p>
             </div>
-            <form className="form" onSubmit={handleCreateNewComment}>
-                <strong>Deixe seu comentário</strong>
 
-                <TextareaCustom message={newComment}
+            <form className="form" onSubmit={handleCreateNewComment}>
+                <strong>Deixe um comentário</strong>
+                <TextareaCustom
+                    message={newComment}
                     setMessage={setNewComment}
-                    title="DIGITE UM COMENTARIO"
+                    title="Deixe um comentários..."
                 />
 
                 <footer>
-                    <button>
-                        Publicar
-                    </button>
+                    <ButtonCustom />
                 </footer>
             </form>
 
             {post.comments?.length && post.comments.map(item => (
-                <Comment key={item.id}comment={item}/>
+                <Comment 
+                    key={item.id} 
+                    comment={item} 
+                    handleDelete={handleDeleteComment} 
+                    handleLike={handleLikeComment} 
+                />
             ))}
         </article>
     )
 }
+
+
